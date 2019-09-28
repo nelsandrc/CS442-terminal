e1d5f14f45612ee2f17ceb33c4cdd6140bbd36e4
nelsandrc <44207981+nelsandrc@users.noreply.github.com>
2019-09-28 15:12:42 -0500

    Create childWrite.cpp
    This is the program for part 1 section A. It passes 3 strings to a child process for writing.

---
+++ childWrite.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#define MSGSIZE 256
+
+int main()
+{
+	std::string strName = "Andrew Nelson";
+	std::string strDpt = "Computer Science";
+	std::string strMail = "nelsona9980@my.uwstout.edu";
+	char* fullName = (char*) strName.c_str();
+	char* department = (char*) strDpt.c_str();
+	char* email = (char*) strMail.c_str();
+
+	char inbuf[MSGSIZE];
+	int p[2], pid, nbytes;
+
+	if (pipe(p) < 0)
+		exit(1);
+
+	if ((pid = fork()) > 0) {
+		write(p[1], fullName, MSGSIZE);
+		write(p[1], department, MSGSIZE);
+		write(p[1], email, MSGSIZE);
+	}
+	else {
+        while ((nbytes = read(p[0], inbuf, MSGSIZE)) > 0) 
+            printf("% s\n", inbuf); 
+        if (nbytes != 0) 
+            exit(2); 
+        printf("Finished reading\n"); 
+    } 
+    return 0; 
+}
