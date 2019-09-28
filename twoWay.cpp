f446f2eaf147133cee6d9da0fd58ff18a7bf9152
nelsandrc <44207981+nelsandrc@users.noreply.github.com>
2019-09-28 18:37:36 -0500

    Update twoWay.cpp
    Completely redesigned program for readability and simpler code. Program writes correctly but does not end. Terminal just waits.

--- twoWay.cpp
+++ twoWay.cpp
@@ -1,61 +1,38 @@
-#include <pthread.h> 
-#include <stdio.h> 
-#include <sys/types.h> 
-#include <unistd.h>  
-#include <stdlib.h>  
-#include <sys/wait.h> 
+#include <cstdio>
+#include <iostream>
 #include <string>
-#define MAX 10 
-  
-int main() 
-{ 
-  
-  int fd[2], i = 0; 
-  pipe(fd); 
-  pid_t pid = fork(); 
-  
-   if(pid > 0) { 
-      wait(NULL); 
+#include <cstring>
+#include <unistd.h>
+#include <sys/types.h>
+#define MSGSIZE 256
 
-		// closing the standard input  
-		close(0); 
+int main()
+{
+	std::string strName = "Andrew Nelson";
+	std::string strDpt = "Computer Science";
+	std::string strMail = "nelsona9980@my.uwstout.edu";
+	char* fullName = (char*) strName.c_str();
+	char* department = (char*) strDpt.c_str();
+	char* email = (char*) strMail.c_str();
 
-		// no need to use the write end of pipe here so close it 
-		close(fd[1]);  
+	char inbuf[MSGSIZE];
+	int p[2], pid, nbytes;
 
-		// duplicating fd[0] with standard input 0 
-		dup(fd[0]);  
-		char* info[MAX]; 
+	if (pipe(p) < 0)
+		exit(1);
 
-		// n stores the total bytes read succesfully 
-		int n = read(fd[0], info, sizeof(info)); 
-		for ( i = 0;i < n/4; i++) 
-
-		// printing the array received from child process 
-		printf("%s ", info[i]);  
-  }  
-  else if( pid == 0 ) { 
-		//Inside child process, pass strings back to parent.
-		char* info[3];
-		std::string strName = "Andrew Nelson";
-		std::string strDpt = "Computer Science";
-		std::string strMail = "nelsona9980@my.uwstout.edu";
-		info[0] = (char*) strName.c_str();
-		info[1] = (char*) strDpt.c_str();
-		info[3]= (char*) strMail.c_str();
-  
-		// no need to use the read end of pipe here so close it 
-		close(fd[0]);  
-
-		// closing the standard output 
-		close(1);     
-
-		// duplicating fd[0] with standard output 1 
-		dup(fd[1]);   
-		write(1, info, sizeof(info)); 
-  }  
-  
-  else { 
-      perror("error\n"); //fork() 
-  } 
-}  
+	if ((pid = fork()) > 0) {
+		while ((nbytes = read(p[0], inbuf, MSGSIZE)) > 0) 
+            printf("% s\n", inbuf); 
+        if (nbytes != 0) 
+            exit(2); 
+        printf("Finished reading\n"); 
+	}
+	else {
+		write(p[1], fullName, MSGSIZE);
+		write(p[1], department, MSGSIZE);
+		write(p[1], email, MSGSIZE);
+        
+    } 
+    return 0; 
+}
