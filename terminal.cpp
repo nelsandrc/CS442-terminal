1bc27256688fe7ce0b89de6699cf3afd0ac87fd9
nelsandrc <44207981+nelsandrc@users.noreply.github.com>
2019-09-30 13:29:37 -0500

    Update terminal.cpp
    Current version of the terminal. Will not compile due to issues with runCommand function.

--- terminal.cpp
+++ terminal.cpp
@@ -7,6 +7,10 @@
 #include <vector>
 #define MAX_SIZE 256
 
+void printHistory(std::vector<std::string> history);
+void runCommand(std::string userSelection);
+
+
 int main()
 {
 	std::string userSelection = "";
@@ -27,58 +31,76 @@ int main()
 
 		}
 		else if (commandName == "pwd") {//Prints the current working directory.
-			std::cout << "PWD\n";
 			char cwd[MAX_SIZE];
 			
 			getcwd(cwd, sizeof(cwd));
 			
 			std::cout <<  "Current working dir: " << cwd << std::endl;
 		}
-		else if (commandName == "helpME") {//Print the command list of nonstandard commands.
+		else if (commandName == "helpMe") {//Print the command list of nonstandard commands.
 			std::cout << "Command List:\n"
-					  << "1: \'history\' - Prints the list of past commands.\n";
+					  << "1: history- Prints the list of past commands.\n";
 		}
 		else if (commandName == "history") {//Prints the list of past commands.
-			std::cout << "History: \n";
-			for(int i = 0; i < history.size(); i++){
-				std::cout << i+1 << ":  " << history[i] << std::endl;
-			}
+			printHistory(history);
 		}
 		else {//Run given command using execvp
-			std::cout << "other command\n";
-			/*pid_t  pid;  
-			char * commandString[3];
-
-			string command="commandName";   // this is the ls unix command
-			string argument1="-l";  // this is an argument for the ls
-
-			commandString[0]=(char*) command.c_str();  //this is how to conver a string variable to a c_string
-			commandString[1]=(char*) argument1.c_str();       
-			commandString[2]=NULL;
-
-			 pid = fork();
-
-			 if (pid == 0) { // a child was created and you are inside it J
-
-					if (execvp(commandString[0], commandString) < 0) {    // try to change the execution impage of the child
-						//here using the execvp, the impage of execution will be replaced for the child by: ls -l. 
-						//If it returns a negative value, that means something went wrong
-						cout<<"*** ERROR: exec failed\n";     
-						exit(1);
-					}
-				cout<<"A New Child was created J \n";  // will not execute this line since  //image of execution has been changed by execvp system call
-			 } 
-			 else if (pid < 0)   {   // no new child was created (fail)
-					cout<<"No New Child Was created L\n"; 
-					return 1;
-			}
-			else // must be the parent
-			{  
-				wait(0); // must wait for a child to finish
-				//cout<<"I am the parent :)\n";   
-			}
-*/
+			runCommand(userSelection);
 		}
 	}
 	return 1;
 }
+
+void printHistory(std::vector<std::string> history){
+	std::cout << "History: \n";
+			for(int i = 0; i < history.size(); i++){
+				std::cout << i+1 << ":  " << history[i] << std::endl;
+			}
+}
+
+void runCommand(std::string userSelection){
+	pid_t  pid;  
+	std::vector<char*> tokenizedString;		
+	char* cstring_conversion = new char[userSelection.length() + 1];
+
+	std::strcpy(cstring_conversion, userSelection.c_str());
+
+	char* currentToken = std::strtok(cstring_conversion, " \n\t\r");
+	
+	while (currentToken != nullptr) {
+		tokenizedCstring.push_back(currentToken);
+		currentToken = std::strtok(NULL, " ,\n\t\r.");
+	}
+
+	for (int i = 0; i < tokenizedCstring.size(); ++i) {
+		std::cout << tokenizedCstring[i] << std::endl;
+	}
+	
+
+	commandString[0]=(char*) command.c_str();  //this is how to conver a string variable to a c_string
+	commandString[1]=(char*) argument1.c_str();       
+	commandString[2]=NULL;
+
+	 pid = fork();
+
+	 if (pid == 0) { // a child was created and you are inside it J
+
+			if (execvp(commandString[0], commandString) < 0) {    // try to change the execution impage of the child
+				//here using the execvp, the impage of execution will be replaced for the child by: ls -l. 
+				//If it returns a negative value, that means something went wrong
+				cout<<"*** ERROR: exec failed\n";     
+				exit(1);
+			}
+		cout<<"A New Child was created J \n";  // will not execute this line since  //image of execution has been changed by execvp system call
+	 } 
+	 else if (pid < 0)   {   // no new child was created (fail)
+			cout<<"No New Child Was created L\n"; 
+			return 1;
+	}
+	else // must be the parent
+	{  
+		wait(0); // must wait for a child to finish
+		//cout<<"I am the parent :)\n";   
+	}
+}
+
